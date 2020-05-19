091901f15f99422b55eda3457f2e9f3746e015ea
Brooks Ambrose <brooksambrose@berkeley.edu>
2020-05-18 22:14:23 -0700

    debug install attempt 1

--- install.R
+++ install.R
@@ -1,32 +1,36 @@
-# if(require(devtools)) {install.packages('devtools');library(devtools)}
-# {devtools::install_github('brooksambrose/pack-dev',subdir='tilit');library(tilit)}
-# if(require(magrittr)) {install.packages('magrittr');library(magrittr)}
-# tilit::ec(
-# 'data.table
-# cowplot
-# magrittr
-# igraph
-# viridisLite
-# viridis
-# RColorBrewer
-# networkD3
-# htmlwidgets
-# crimCV
-# ggplot2
-# cluster
-# dplyr
-# gghighlight
-# tidyr
-# ggrepel
-# directlabels
-# factoextra
-# ggthemes
-# skmeans
-# slam
-# ggnetwork
-# network
-# sna',s='\n') %>% {
-# inst<-setdiff(.,installed.packages() %>% rownames)
-# #load<-intersect(.,installed.packages() %>% rownames)
-# install.packages(.,Ncpus=parallel::detectCores()-1)
-# }
+lib<-path.expand('~/.local/R')
+dir.create(lib)
+Sys.setenv(R_LIBS=paste(lib,Sys.getenv('R_LIBS'),sep=':'),R_LIBS_USER=lib)
+.libPaths(Sys.getenv("R_LIBS_USER"))
+if(!require(devtools)) {install.packages('devtools');library(devtools)}
+if(!require(kableExtra)) {install.packages('kableExtra')}
+{devtools::install_github('brooksambrose/pack-dev',subdir='tilit',lib=lib);library(tilit)}
+if(!require(magrittr)) {install.packages('magrittr');library(magrittr)}
+tilit::ec(
+  'data.table
+cowplot
+igraph
+viridisLite
+viridis
+RColorBrewer
+networkD3
+htmlwidgets
+crimCV
+ggplot2
+cluster
+dplyr
+gghighlight
+tidyr
+ggrepel
+directlabels
+factoextra
+ggthemes
+skmeans
+slam
+ggnetwork
+network
+sna',s='\n') %>% {
+  inst<-setdiff(.,installed.packages() %>% rownames)
+  #load<-intersect(.,installed.packages() %>% rownames)
+  install.packages(inst,Ncpus=parallel::detectCores()-1)
+}
