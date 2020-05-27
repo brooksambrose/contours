2bad5bb6c04471231feae804f8375fed20f6d1bf
Brooks Ambrose <brooksambrose@berkeley.edu>
2020-05-26 17:41:02 -0700

    add plagiat likely to fail due to unknown sysreqs

--- Dockerfile
+++ Dockerfile
@@ -9,14 +9,7 @@ RUN mkdir ${LOCR}
 ENV R_LIBS_SITE ${LOCR}
 
 # install system requirements first
-USER root
-RUN apt-get update \
-  && apt-get install -y --no-install-recommends \
-    libpoppler-cpp-dev `#pdftools` \
-  && apt-get purge \
-  && apt-get clean \
-  && rm -rf /var/lib/apt/lists/*
-USER ${NB_USER}
+USER rootRUN apt-get update \  && apt-get install -y --no-install-recommends \   #data.table    zlib1g-dev \   #pdftools    libpoppler-cpp-dev \   #tesseract    libtesseract-dev libleptonica-dev tesseract-ocr-eng \   #igraph   #xml2    libxml2-dev \  && apt-get purge \  && apt-get clean \  && rm -rf /var/lib/apt/lists/*USER ${NB_USER}
 
 ## Run an install.R script, if it exists.
 RUN if [ -f install.R ]; then R --quiet -f install.R; fi
--- install.R
+++ install.R
@@ -1,14 +1,11 @@
-# lib<-path.expand('~/.local/R')
-# dir.create(lib)
-# Sys.setenv(R_LIBS=paste(lib,Sys.getenv('R_LIBS'),sep=':'),R_LIBS_USER=lib)
-# .libPaths(Sys.getenv("R_LIBS_USER"))
-if(!require(devtools)) {install.packages('devtools');library(devtools)}
-#if(!require(kableExtra)) {install.packages('kableExtra')}
-{devtools::install_github(
-  'brooksambrose/pack-dev',subdir='tilit',dependencies=T
-  #,lib=lib
-);library(tilit)}
-if(!require(magrittr)) {install.packages('magrittr');library(magrittr)}
+nc<-max(1,parallel::detectCores()-1)
+if(!require(devtools)) {install.packages('devtools',Ncpus=nc);library(devtools)}
+# special packages
+devtools::install_github('bstewart/stm',ref = 'development',Ncpus=nc)
+{devtools::install_github('brooksambrose/pack-dev',subdir='tilit',Ncpus=nc);library(tilit)}
+devtools::install_github('brooksambrose/pack-dev',subdir = 'plagiat',Ncpus=nc)
+# CRAN packages
+if(!require(magrittr)) {install.packages('magrittr',Ncpus=nc);library(magrittr)}
 tilit::ec(
 'data.table
 epubr
@@ -37,7 +34,5 @@ network
 sna',s='\n') %>% {
   inst<-setdiff(.,installed.packages() %>% rownames)
   #load<-intersect(.,installed.packages() %>% rownames)
-  install.packages(inst,Ncpus=max(1,parallel::detectCores()-1))
+  install.packages(inst,Ncpus=nc)
 }
-# special packages
-devtools::install_github('bstewart/stm',ref = 'development')
